802c37824f881fa842ff86cc84f14421871d79c4
Sora <dart238@email.com>
2023-12-02 16:13:20 -0500

    Defining function to populate slots machine at random

---
+++ script.js
@@ -0,0 +1,49 @@
+//Variables
+let score = 0
+let spin = 0 
+
+const slotArray = [1, 2, 3, 4, 5, 6, 7]
+
+
+//Cached Elements
+let slotRow = document.getElementsByClassName('row1')
+let slotRow2 = document.getElementsByClassName('row2')
+let slotRow3 = document.getElementsByClassName('row3')
+
+//Functions
+
+function fillSlotRow(row) {
+    let fillArray = slotArray
+
+    function randomValues() {
+        const randomIndex = Math.floor(Math.random() * fillArray.length);
+        const value = fillArray[randomIndex];
+        
+        fillArray.splice(randomIndex, 1);
+        return value;
+    }
+    const slotValue1 = randomValues()
+    //const slotValue2 = randomValues()
+    //const slotValue3 = randomValues()
+    console.log(slotValue1)
+
+
+    console.log(row.children)
+    //const faSlotValue1 = row.
+    //const faSlotValue2 = row.querySelector('nth-child(2)')
+    //const faSlotValue3 = row.querySelector('nth-child(3)')
+
+
+    //faSlotValue1.innerHTML = slotValue1
+    //faSlotValue2.innerHTML = slotValue2
+    //faSlotValue3.innerHTML = slotValue3
+    }
+
+    // function iniit () {
+    //     fillSlotRow(slotRow)
+    //     fillSlotRow(slotRow2)
+    //     fillSlotRow(slotRow3)
+    // }
+
+
+    
