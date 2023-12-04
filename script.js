3178eb6d1daf10b84c36786d8cb31ea764db30b3
Sora <dart238@email.com>
2023-12-03 19:08:52 -0500

    fixed issue with fillSlotRow function, wasnt filling the board after reset function was ran

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 //Variables
 let score = 0
-let spin = 0 
+let spin = 0
 
 const slotArray = [1, 2, 3, 4, 5, 6, 7]
+
 
 
 //Cached Elements
@@ -13,33 +14,33 @@ let slotRow3 = document.querySelector('.row3')
 //Functions
 
 function fillSlotRow(row) {
-    let fillArray = slotArray
+    let fillArray = [1, 2, 3, 4, 5, 6, 7]
     let count = 0
 
     function randomValues() {
         const randomIndex = Math.floor(Math.random() * fillArray.length);
         const value = fillArray[randomIndex];
-        count +=1
+        count += 1
 
-        if (count === 3){
+        if (count === 3) {
             fillArray = [1, 2, 3, 4, 5, 6, 7]
             count = 0
         }
-        
+
         fillArray.splice(randomIndex, 1);
         return value;
     }
     const slotValue1 = randomValues()
     const slotValue2 = randomValues()
     const slotValue3 = randomValues()
+
+
+
     
-
-
-    console.log(row.firstElementChild)
     let faSlotValue1 = row.querySelector(':first-child')
     let faSlotValue2 = row.querySelector(':nth-child(2)')
     let faSlotValue3 = row.querySelector(':nth-child(3)')
-    
+
 
     faSlotValue1.innerHTML = slotValue1
     faSlotValue2.innerHTML = slotValue2
@@ -48,20 +49,28 @@ function fillSlotRow(row) {
     console.log(count)
     console.log(fillArray)
 
-    }
+}
 
-    function resetSlots() {
-       let clearSlots = document.querySelectorAll('.square') 
-       clearSlots.forEach(function(slot){
+function resetSlots() {
+    let clearSlots = document.querySelectorAll('.square')
+    clearSlots.forEach(function (slot) {
         slot.innerHTML = ''
-       })
+    })
+}
+
+function calculateScore() {
+    let row1 = slotRow.querySelector(':first-child').innerHTML
+    let row2 = slotRow2.querySelector(':nth-child(2)').innerHTML
+    let row3 = slotRow3.querySelector(':nth-child(3)').innerHTML
+    if (row1 === row2 && row1 === row3) {
+        score += row1
     }
+}
 
-    function iniit () {
-        fillSlotRow(slotRow)
-        fillSlotRow(slotRow2)
-        fillSlotRow(slotRow3)
-    }
+function iniit() {
+    fillSlotRow(slotRow)
+    fillSlotRow(slotRow2)
+    fillSlotRow(slotRow3)
+}
 
 
-    
